42b7f5118c048d20d26888ecbde4fcc71f86be45
OscarANilsson <erik-oscar-nilsson@live.se>
2017-09-08 21:46:58 +0200

    Fixed some of the mistakes that I did.

--- matlab/Homework_2.m
+++ matlab/Homework_2.m
@@ -4,11 +4,12 @@ veckholder=zeros(rowcol(2),1);
 for i=1:rowcol(2)
     %display(A(:,i));
     if i>=2
-        for j=1:i
-            veckholder=veckholder-dot(A(:,j),A(:,i))/dot(A(:,j),A(:,j))*A(:,j);
+        for j=1:i-1
+            veckholder=veckholder-dot(A(:,j),A(:,i))/dot(A(:,j),A(:,j))*A(:,i-1);
         end
     else
         A(:,i)=A(:,i);
     end
-    A(:,i)=A(:,i)-veckholder;
+    A(:,i)=A(:,i)+veckholder;
 end
+A
