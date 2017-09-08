372dd33338e28ac89c217b8ac7ed5dc39f5fc86b
OscarANilsson <erik-oscar-nilsson@live.se>
2017-09-08 21:26:27 +0200

    Trying to be more comfortable with github and refresh my memory in matlab.

---
+++ matlab/Homework_2.m
@@ -0,0 +1,14 @@
+A=[1 2; 0 1];
+rowcol=size(A);
+veckholder=zeros(rowcol(2),1);
+for i=1:rowcol(2)
+    %display(A(:,i));
+    if i>=2
+        for j=1:i
+            veckholder=veckholder-dot(A(:,j),A(:,i))/dot(A(:,j),A(:,j))*A(:,j);
+        end
+    else
+        A(:,i)=A(:,i);
+    end
+    A(:,i)=A(:,i)-veckholder;
+end
