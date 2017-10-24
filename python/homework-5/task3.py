88a49c7fabf4d0aff32f7afb0d0fbcf3e8b96c3f
OscarANilsson <erik-oscar-nilsson@live.se>
2017-10-24 12:57:21 +0200

    Fixed task3 in Homework-5.

--- python/homework-5/task3.py
+++ python/homework-5/task3.py
@@ -11,12 +11,12 @@ import scipy.linalg as sl
 
 def count_eig_negative(A, x):
     """
-    We compute the sub matrices of A, to compute the number of negative
-    eigenvalues by counting the number of sign changes.
+    We compute the sub-matrices of A - xI, to compute the number of
+    negative eigenvalues by counting the number of sign changes.
 
     :param A: a square matrix
     :type A: np.ndarray
-    :param x: evaluation point for p(x)
+    :param x: shift variable
     :type x: float
     """
 
@@ -47,9 +47,11 @@ def count_eig_between(A, a, b):
     # Deflate the problem if possible.
     # Otherwise just invoke count_eig_negative.
     m = A.shape[0]
+
     # Handle the trivial case of a 1x1 matrix.
     if m == 1:
         return [A[0, 0]], 0
+
     # Boolean array representing the superdiagonal elements.
     super_diagonal = np.tri(m, k=1, dtype=bool) - np.tri(m, dtype=bool)
     isclose = list(np.isclose(A[super_diagonal], 0))
@@ -57,8 +59,9 @@ def count_eig_between(A, a, b):
     if j > 0:
         return count_eig_between(A[:j+1, :j+1], a, b) + \
                count_eig_between(A[j+1:, j+1:], a, b)
-    # Here we remember that we count the number of eigenvalues less than a,
-    # then we take away the interval less than b, i.e. (b, A].
+
+    # Here count the  eigenvalues less than or equal to
+    # b, then we take away the interval less than a , i.e. (b, A].
     return count_eig_negative(A, b) - count_eig_negative(A, a)
 
 def find_eig_between(A, a, b, atol = 1.e-8):
@@ -91,17 +94,17 @@ if __name__ == '__main__':
         Generate a symmetric random mxm matrix.
 
         :param m: the desired number of rows and columns
-        :type m: integer
+        :type m: int
         """
         A = nr.rand(m, m)
         return (A + A.T)/2
 
     def random_tridiagonal_matrix(m):
         """
-        Generate a symetric trididagonal matrix
+        Generate a symetric trididagonal matrix.
 
         :param m: the desired dimension of the square matri x.
-        :type m: integer
+        :type m: int 
         """
         return sl.hessenberg(random_symmetric_matrix(m))
 
@@ -112,7 +115,7 @@ if __name__ == '__main__':
         :param n_many_times: number of random matrices we test.
         :type n_many_times: integer
         :param m: size of the matrices we are testing.
-        :type m: integer
+        :type m: int
         """
         for i in range(n_many_tests):
             A = random_tridiagonal_matrix(m)
@@ -130,4 +133,4 @@ if __name__ == '__main__':
     # Test all methods.
     test_count_eig_negative(100, 10)
     # FIXME: test count_eig_between
-    # FIXME: test find_eig_between
+    # test find_eig_between
